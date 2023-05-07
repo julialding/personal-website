c513013342520d10c7d221220e69bbadc0018c5e
chndr1 <bhargav.annem@gmail.com>
2023-05-07 17:08:05 -0500

    fix: unescaped entities patched

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,22 +1,31 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import ProjectItem from './ProjectItem';
-import juliaDing from '../public/assets/juliading23.JPG';
+import React from "react";
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full'>
-      <div className='max-w-[1240px] mx-auto px-6 py-16'>
-        <p className='text-xl tracking-widest uppercase text-sky-700'>
-          03
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-6 py-16">
+        <p className="text-xl tracking-widest uppercase text-sky-700">03</p>
+        <h2 className="py-4">
+          Projects<span className="text-sky-700">.</span>
+        </h2>
+        <p className="group py-2 text-zinc-600">
+          Project grid coming soon! In the meantime, check out my<span> </span>
+          <a
+            className="underline underline-offset-2 decoration-2 decoration-sky-700 hover:decoration-[3px] hover:decoration-sky-800"
+            href="https://linkedin.com/in/julialding/"
+          >
+            LinkedIn
+          </a>
+          <span> </span>
+          or{" "}
+          <a
+            className="underline underline-offset-2 decoration-2 decoration-sky-700 hover:decoration-[3px] hover:decoration-sky-800"
+            href="https://github.com/julialding/"
+          >
+            Github
+          </a>
+          .<span> </span>
         </p>
-        <h2 className='py-4'>Projects<span className='text-sky-700'>.</span></h2>
-        <p className='group py-2 text-zinc-600'>
-            Project grid coming soon! In the meantime, check out my<span> </span>
-            <a className="underline underline-offset-2 decoration-2 decoration-sky-700 hover:decoration-[3px] hover:decoration-sky-800" href="https://linkedin.com/in/julialding/">LinkedIn</a><span> </span>
-            or <a className="underline underline-offset-2 decoration-2 decoration-sky-700 hover:decoration-[3px] hover:decoration-sky-800" href="https://github.com/julialding/">Github</a>.<span> </span>
-          </p>
       </div>
     </div>
   );
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,3 @@
 export default function About() {
-    return (
-      <div>this is me</div>
-    )
-  }
+  return <div>Bhargav was here</div>;
+}
