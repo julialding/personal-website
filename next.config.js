db803a26c2de5e64fffb246537fccdba201a40bb
chndr1 <bhargav.annem@gmail.com>
2023-05-07 18:17:08 -0500

    refactor: module exports can be empty

--- next.config.js
+++ next.config.js
@@ -1,12 +1,2 @@
 
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/test',
-        destination: '/main',
-        permanent: true,
-      },
-    ]
-  },
-}
+module.exports = {}
